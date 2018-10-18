83ce3795ebaa00d42e7fa9c5ebeb287847764901
James Willans <james.willans@accordmarketing.com>
2018-10-18 16:08:34 +0100

    Started work on Doctrine entity generator

---
+++ src/CodeGenerator/CodeGeneratorInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kyoushu\Conjuration\CodeGenerator;
+
+interface CodeGeneratorInterface
+{
+
+    public function generate(): string;
+
+}
---
+++ src/CodeGenerator/DoctrineEntityGenerator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Kyoushu\Conjuration\CodeGenerator;
+
+class DoctrineEntityGenerator implements CodeGeneratorInterface
+{
+
+    public function __construct(string $namespace)
+    {
+    }
+
+    public function generate(): string
+    {
+        // TODO: Implement generate() method.
+    }
+
+}
